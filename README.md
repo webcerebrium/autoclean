265496c8679cd0dff6c66ca55a15c6c6c3c595dd
wcrbrm <webcerebrium@gmail.com>
2018-02-02 10:02:45 +0200

    autoclean files by expiration

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # autoclean
-Command Line auto cleaner - for removing expired files
+Simple command line auto cleaner - for removing expired files
+
+## Usage
+```
+autoclean --days=30 --dry-run=1 /tmp/mypath
+```
+
+## License
+MIT
---
+++ bin.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+
+const fs = require('fs-extra');
+const path = require('path');
+const parseArgs = require('minimist');
+
+// helpers to get parameters:
+const commandArgs = parseArgs(process.argv, { '--': true });
+const isVerbose = () => (commandArgs.v || commandArgs.verbose);
+const isDryRun = () => [1, '1'].indexOf(commandArgs['dry-run'] || commandArgs.dryRun) !== 1;
+const getDays = () => (commandArgs.d || commandArgs.days);
+const getMaxDepth = () => (commandArgs['max-depth'] || commandArgs.maxDepth || 1);
+const getPath = () => (commandArgs.path || commandArgs._[2]);
+
+const usage = () => {
+  console.log('USAGE: autoclean --days=30 --dry-run=1 /tmp/mypath');
+  process.exit(0);
+};
+
+if (!getPath() || !getDays()) { usage(); }
+if (isVerbose()) {
+  console.log('PATH: ', getPath());
+  console.log('DAYS: ', getDays());
+  console.log('MAX DEPTH: ', getMaxDepth());
+}
+console.log('DRY RUN: ', isDryRun());
+
+const filelist = [];
+const dirlist = [];
+const maxTime = new Date().getTime() - 3600000*24*getDays();
+
+const walkSync = (dir) => {
+  const files = fs.readdirSync(dir);
+  files.forEach((file) => {
+    const filePath = dir + path.sep + file;
+    const stat = fs.statSync(filePath);
+    if (stat.mtime < maxTime) {
+      if (stat.isDirectory()) {
+        walkSync(filePath);
+        dirlist.push(filePath);
+      } else {
+        filelist.push(filePath);
+      }
+    }
+  });
+};
+
+walkSync(getPath());
+
+filelist.forEach(file => {
+  console.log('FILE MATCH: ', file, ' ', new Date(fs.statSync(file).mtime));
+  if (!isDryRun()) { fs.removeSync(file); } 
+});
+dirlist.forEach(dir => {
+  console.log('DIR MATCH: ', dir, ' ', new Date(fs.statSync(dir).mtime));
+  if (!isDryRun()) { fs.removeSync(dir); } 
+});
+
